74013feddb5e4a5b37a6bafda12b080a30d75ad6
Thiện Đỗ <thiendo3831@gmail.com>
2022-02-06 22:37:24 +0700

    feat: setup folder structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@app': './src/app',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@containers': './src/containers',
+          '@core': './src/core',
+          '@hooks': './src/hooks',
+          '@i18n': './src/i18n',
+          '@navigation': './src/navigation',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@theme': './src/theme',
+          '@utilities': './src/utilities',
+        },
+      },
+    ],
+  ],
 };
